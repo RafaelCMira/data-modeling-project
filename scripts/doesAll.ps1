944fe5c9e881f5a5d448a46b7cd866b1c959d478
RafaelCMira <113689920+RafaelCMira@users.noreply.github.com>
2024-11-30 17:30:57 +0000

    Improved scripts to generate execution times

--- scripts/doesAll.ps1
+++ scripts/doesAll.ps1
@@ -1,6 +1,7 @@
 param (
     [string]$pgPassword,
-    [string]$datasetFolder
+    [string]$datasetFolder,
+    [string]$outputFile = "output.txt"  # Optional parameter for the output file
 )
 
 if (-not $pgPassword) {
@@ -13,20 +14,26 @@ if (-not $datasetFolder) {
     exit 1
 }
 
+Write-Output "-------------------------------------------------------------------------------"
 
-# 1. Runs the py scripts
+ # 1. Runs the py scripts
+$entitiesTime = Measure-Command {
+    python "../python/parseEntities.py" --dataset_folder $datasetFolder
+}
+Write-Output "Entities scripts completed in $($entitiesTime.TotalSeconds) seconds."
 
-python "../python/parseEntities.py" --dataset_folder $datasetFolder
-python "../python/parseRelations.py" --dataset_folder $datasetFolder
+$relationsTime = Measure-Command {
+    python "../python/parseRelations.py" --dataset_folder $datasetFolder
+}
+Write-Output "Relations scripts completed in $($relationsTime.TotalSeconds) seconds."
+
 
 # # 2. Copies the sql files to "C:\temp" directory
-
 Copy-Item "../systems/postgreSQL/schema.sql" "C:\temp"
 Copy-Item "../systems/postgreSQL/bulkInserts.sql" "C:\temp"
 Copy-Item "../systems/postgreSQL/constraints.sql" "C:\temp"
 
 # 3. Runs this commands:
-
 $env:PGPASSWORD=$pgPassword
 
 $schemaTime = Measure-Command {
@@ -48,3 +55,5 @@ $analyzeTime = Measure-Command {
     psql -U postgres -d test -c "ANALYZE;"
 }
 Write-Output "ANALYZE completed in $($analyzeTime.TotalSeconds) seconds."
+
+Write-Output "-------------------------------------------------------------------------------"
