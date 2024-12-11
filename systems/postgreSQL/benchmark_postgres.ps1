0d3c4102edfdc3d8eeb0d158a22ee57764b11e04
RafaelCMira <113689920+RafaelCMira@users.noreply.github.com>
2024-12-11 12:17:05 +0000

    .

--- systems/postgreSQL/benchmark_postgres.ps1
+++ systems/postgreSQL/benchmark_postgres.ps1
@@ -59,110 +59,110 @@ jmeter -n -t "../../inputs/dataset $dataset/jmetter_postgres_$dataset.jmx" -l $F
     
 
 
-# # -------------------- 1GB PostgreSQL configuration Benchmark --------------------
-# $FullOutputDir = Resolve-Path -Path $OutputDir
-# $FullOutputDir = Join-Path -Path $FullOutputDir.Path -ChildPath "1GB.csv"
+# -------------------- 1GB PostgreSQL configuration Benchmark --------------------
+$FullOutputDir = Resolve-Path -Path $OutputDir
+$FullOutputDir = Join-Path -Path $FullOutputDir.Path -ChildPath "1GB.csv"
 
-# $replacementFilePath = "./configs/postgresql_1GB_config.conf"
+$replacementFilePath = "./configs/postgresql_1GB_config.conf"
 
-# Copy-Item -Path $replacementFilePath -Destination $originalFilePath -Force
+Copy-Item -Path $replacementFilePath -Destination $originalFilePath -Force
 
-# # Restart the PostgreSQL service
-# Restart-Service -Name "postgresql-x64-17"
+# Restart the PostgreSQL service
+Restart-Service -Name "postgresql-x64-17"
 
-# Start-Sleep -Seconds 3
+Start-Sleep -Seconds 3
 
-# # Run Jmeter
-# jmeter -n -t "../../inputs/dataset $dataset/jmetter_postgres_$dataset.jmx" -l $FullOutputDir `
-#     -Jcsv1="../../inputs/dataset $dataset/1.csv" `
-#     -Jcsv2="../../inputs/dataset $dataset/2.csv" `
-#     -Jcsv3="../../inputs/dataset $dataset/3.csv" `
-#     -Jcsv4="../../inputs/dataset $dataset/4.csv" `
-#     -Jcsv5="../../inputs/dataset $dataset/5.csv" `
-#     -Jcsv6="../../inputs/dataset $dataset/6.csv" `
-#     -Jcsv7="../../inputs/dataset $dataset/7.csv" `
-#     -Jcsv8="../../inputs/dataset $dataset/8.csv" `
-#     -Jcsv9="../../inputs/dataset $dataset/9.csv" `
-#     -Jcsv10="../../inputs/dataset $dataset/10.csv"
-
-
-# # -------------------- 2GB PostgreSQL configuration Benchmark --------------------
-# $FullOutputDir = Resolve-Path -Path $OutputDir
-# $FullOutputDir = Join-Path -Path $FullOutputDir.Path -ChildPath "2GB.csv"
-
-# $replacementFilePath = "./configs/postgresql_2GB_config.conf"
-
-# Copy-Item -Path $replacementFilePath -Destination $originalFilePath -Force
-
-# # Restart the PostgreSQL service
-# Restart-Service -Name "postgresql-x64-17"
-
-# Start-Sleep -Seconds 3
-
-# # Run Jmeter
-# jmeter -n -t "../../inputs/dataset $dataset/jmetter_postgres_$dataset.jmx" -l $FullOutputDir `
-#     -Jcsv1="../../inputs/dataset $dataset/1.csv" `
-#     -Jcsv2="../../inputs/dataset $dataset/2.csv" `
-#     -Jcsv3="../../inputs/dataset $dataset/3.csv" `
-#     -Jcsv4="../../inputs/dataset $dataset/4.csv" `
-#     -Jcsv5="../../inputs/dataset $dataset/5.csv" `
-#     -Jcsv6="../../inputs/dataset $dataset/6.csv" `
-#     -Jcsv7="../../inputs/dataset $dataset/7.csv" `
-#     -Jcsv8="../../inputs/dataset $dataset/8.csv" `
-#     -Jcsv9="../../inputs/dataset $dataset/9.csv" `
-#     -Jcsv10="../../inputs/dataset $dataset/10.csv"
+# Run Jmeter
+jmeter -n -t "../../inputs/dataset $dataset/jmetter_postgres_$dataset.jmx" -l $FullOutputDir `
+    -Jcsv1="../../inputs/dataset $dataset/1.csv" `
+    -Jcsv2="../../inputs/dataset $dataset/2.csv" `
+    -Jcsv3="../../inputs/dataset $dataset/3.csv" `
+    -Jcsv4="../../inputs/dataset $dataset/4.csv" `
+    -Jcsv5="../../inputs/dataset $dataset/5.csv" `
+    -Jcsv6="../../inputs/dataset $dataset/6.csv" `
+    -Jcsv7="../../inputs/dataset $dataset/7.csv" `
+    -Jcsv8="../../inputs/dataset $dataset/8.csv" `
+    -Jcsv9="../../inputs/dataset $dataset/9.csv" `
+    -Jcsv10="../../inputs/dataset $dataset/10.csv"
 
 
-# # -------------------- 4GB PostgreSQL configuration Benchmark --------------------
-# $FullOutputDir = Resolve-Path -Path $OutputDir
-# $FullOutputDir = Join-Path -Path $FullOutputDir.Path -ChildPath "4GB.csv"
+# -------------------- 2GB PostgreSQL configuration Benchmark --------------------
+$FullOutputDir = Resolve-Path -Path $OutputDir
+$FullOutputDir = Join-Path -Path $FullOutputDir.Path -ChildPath "2GB.csv"
 
-# $replacementFilePath = "./configs/postgresql_4GB_config.conf"
+$replacementFilePath = "./configs/postgresql_2GB_config.conf"
 
-# Copy-Item -Path $replacementFilePath -Destination $originalFilePath -Force
+Copy-Item -Path $replacementFilePath -Destination $originalFilePath -Force
 
-# # Restart the PostgreSQL service
-# Restart-Service -Name "postgresql-x64-17"
+# Restart the PostgreSQL service
+Restart-Service -Name "postgresql-x64-17"
 
-# Start-Sleep -Seconds 3
+Start-Sleep -Seconds 3
 
-# # Run Jmeter
-# jmeter -n -t "../../inputs/dataset $dataset/jmetter_postgres_$dataset.jmx" -l $FullOutputDir `
-#     -Jcsv1="../../inputs/dataset $dataset/1.csv" `
-#     -Jcsv2="../../inputs/dataset $dataset/2.csv" `
-#     -Jcsv3="../../inputs/dataset $dataset/3.csv" `
-#     -Jcsv4="../../inputs/dataset $dataset/4.csv" `
-#     -Jcsv5="../../inputs/dataset $dataset/5.csv" `
-#     -Jcsv6="../../inputs/dataset $dataset/6.csv" `
-#     -Jcsv7="../../inputs/dataset $dataset/7.csv" `
-#     -Jcsv8="../../inputs/dataset $dataset/8.csv" `
-#     -Jcsv9="../../inputs/dataset $dataset/9.csv" `
-#     -Jcsv10="../../inputs/dataset $dataset/10.csv"
+# Run Jmeter
+jmeter -n -t "../../inputs/dataset $dataset/jmetter_postgres_$dataset.jmx" -l $FullOutputDir `
+    -Jcsv1="../../inputs/dataset $dataset/1.csv" `
+    -Jcsv2="../../inputs/dataset $dataset/2.csv" `
+    -Jcsv3="../../inputs/dataset $dataset/3.csv" `
+    -Jcsv4="../../inputs/dataset $dataset/4.csv" `
+    -Jcsv5="../../inputs/dataset $dataset/5.csv" `
+    -Jcsv6="../../inputs/dataset $dataset/6.csv" `
+    -Jcsv7="../../inputs/dataset $dataset/7.csv" `
+    -Jcsv8="../../inputs/dataset $dataset/8.csv" `
+    -Jcsv9="../../inputs/dataset $dataset/9.csv" `
+    -Jcsv10="../../inputs/dataset $dataset/10.csv"
+
+
+# -------------------- 4GB PostgreSQL configuration Benchmark --------------------
+$FullOutputDir = Resolve-Path -Path $OutputDir
+$FullOutputDir = Join-Path -Path $FullOutputDir.Path -ChildPath "4GB.csv"
+
+$replacementFilePath = "./configs/postgresql_4GB_config.conf"
+
+Copy-Item -Path $replacementFilePath -Destination $originalFilePath -Force
+
+# Restart the PostgreSQL service
+Restart-Service -Name "postgresql-x64-17"
+
+Start-Sleep -Seconds 3
+
+# Run Jmeter
+jmeter -n -t "../../inputs/dataset $dataset/jmetter_postgres_$dataset.jmx" -l $FullOutputDir `
+    -Jcsv1="../../inputs/dataset $dataset/1.csv" `
+    -Jcsv2="../../inputs/dataset $dataset/2.csv" `
+    -Jcsv3="../../inputs/dataset $dataset/3.csv" `
+    -Jcsv4="../../inputs/dataset $dataset/4.csv" `
+    -Jcsv5="../../inputs/dataset $dataset/5.csv" `
+    -Jcsv6="../../inputs/dataset $dataset/6.csv" `
+    -Jcsv7="../../inputs/dataset $dataset/7.csv" `
+    -Jcsv8="../../inputs/dataset $dataset/8.csv" `
+    -Jcsv9="../../inputs/dataset $dataset/9.csv" `
+    -Jcsv10="../../inputs/dataset $dataset/10.csv"
 
 
 
-# # -------------------- 8GB PostgreSQL configuration Benchmark --------------------
-# $FullOutputDir = Resolve-Path -Path $OutputDir
-# $FullOutputDir = Join-Path -Path $FullOutputDir.Path -ChildPath "8GB.csv"
+# -------------------- 8GB PostgreSQL configuration Benchmark --------------------
+$FullOutputDir = Resolve-Path -Path $OutputDir
+$FullOutputDir = Join-Path -Path $FullOutputDir.Path -ChildPath "8GB.csv"
 
-# $replacementFilePath = "./configs/postgresql_8GB_config.conf"
+$replacementFilePath = "./configs/postgresql_8GB_config.conf"
 
-# Copy-Item -Path $replacementFilePath -Destination $originalFilePath -Force
+Copy-Item -Path $replacementFilePath -Destination $originalFilePath -Force
 
-# # Restart the PostgreSQL service
-# Restart-Service -Name "postgresql-x64-17"
+# Restart the PostgreSQL service
+Restart-Service -Name "postgresql-x64-17"
 
-# Start-Sleep -Seconds 3
+Start-Sleep -Seconds 3
 
-# # Run Jmeter
-# jmeter -n -t "../../inputs/dataset $dataset/jmetter_postgres_$dataset.jmx" -l $FullOutputDir `
-#     -Jcsv1="../../inputs/dataset $dataset/1.csv" `
-#     -Jcsv2="../../inputs/dataset $dataset/2.csv" `
-#     -Jcsv3="../../inputs/dataset $dataset/3.csv" `
-#     -Jcsv4="../../inputs/dataset $dataset/4.csv" `
-#     -Jcsv5="../../inputs/dataset $dataset/5.csv" `
-#     -Jcsv6="../../inputs/dataset $dataset/6.csv" `
-#     -Jcsv7="../../inputs/dataset $dataset/7.csv" `
-#     -Jcsv8="../../inputs/dataset $dataset/8.csv" `
-#     -Jcsv9="../../inputs/dataset $dataset/9.csv" `
-#     -Jcsv10="../../inputs/dataset $dataset/10.csv"
+# Run Jmeter
+jmeter -n -t "../../inputs/dataset $dataset/jmetter_postgres_$dataset.jmx" -l $FullOutputDir `
+    -Jcsv1="../../inputs/dataset $dataset/1.csv" `
+    -Jcsv2="../../inputs/dataset $dataset/2.csv" `
+    -Jcsv3="../../inputs/dataset $dataset/3.csv" `
+    -Jcsv4="../../inputs/dataset $dataset/4.csv" `
+    -Jcsv5="../../inputs/dataset $dataset/5.csv" `
+    -Jcsv6="../../inputs/dataset $dataset/6.csv" `
+    -Jcsv7="../../inputs/dataset $dataset/7.csv" `
+    -Jcsv8="../../inputs/dataset $dataset/8.csv" `
+    -Jcsv9="../../inputs/dataset $dataset/9.csv" `
+    -Jcsv10="../../inputs/dataset $dataset/10.csv"
